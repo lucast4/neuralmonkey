2f36290442bbf458428a5d2b053e9999bc60c6bf
Lucas <lucasytian@gmail.com>
2023-08-26 17:50:17 -0400

    minor

--- neuralmonkey/kilosort/utils/datstruct_save.m
+++ neuralmonkey/kilosort/utils/datstruct_save.m
@@ -1,4 +1,6 @@
-function datstruct_save(DATSTRUCT, savedir, suffix)
+function datstruct_save(DATSTRUCT, savedir, suffix, also_save_each_unit)
+
+if ~exist('also_save_each_unit', 'var'); also_save_each_unit = false; end
 
 if length(suffix)>0
     name = ['DATSTRUCT_' suffix];
@@ -21,4 +23,33 @@ for i=1:length(cols_remove)
 end
 % DATSTRUCT_SORTED = table2struct(sortedT); % change it back to struct array if necessary
 writetable(sortedT, [savedir '/' name '-table.csv']);
+
+% Save each unit (item in DATSTRUCT), first ordered by global chan.
+if also_save_each_unit
+	disp('Saving separate4ly each unit...')
+
+	% save each cluster (i.e., a unit)
+	savedir = [savedir '/dat_individual'];
+	mkdir(savedir)
+
+	% 1) Sort by chan global, so that "unit" is in order
+	T = struct2table(DATSTRUCT); % convert the struct array to a table
+	sortedT = sortrows(T, 'chan_global'); % sort the table by 'DOB'
+	DATSTRUCT_SORTED = table2struct(sortedT); % change it back to struct array if necessary
+
+	for i=1:length(DATSTRUCT_SORTED)
+	    dat = DATSTRUCT_SORTED(i);
+	    path = [savedir '/unit_' num2str(i) '.mat'];
+	    % chg = dat.chan_global;
+	    % cl = dat.clust;
+	%     path = [savedir '/unit_' num2str(i) chan_global_' num2str(chg) '-clust_' num2str(cl) '.mat'];
+	    save(path, 'dat');
+	    % disp(path)
+	    % assert(false);
+	    if mod(i, 20)==0
+	    	disp(i)
+	    end
+	end
+end
+
 end
---
+++ neuralmonkey/kilosort/utils/lt_plot_format.m
@@ -0,0 +1,9 @@
+%% 4/28/15 - Formats plot aesthetics (e.g. font size)
+
+
+set(gcf,'Color',[0.9 0.9 0.9])
+% set(gca,'FontSize',12,'FontWeight','bold');
+set(gca,'FontSize',10);
+set(gca, 'Color', [0.9 0.9 0.9])
+box off;
+
