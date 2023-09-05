20b80f3d5050170dff684b3573030b1dcf90c5cf
Lucas <lucasytian@gmail.com>
2023-09-05 18:05:14 -0400

    ks postprocess, more options

--- neuralmonkey/kilosort/kspostprocess_CURATE.m
+++ neuralmonkey/kilosort/kspostprocess_CURATE.m
@@ -1,6 +1,7 @@
 clear all; close all; 
 ANIMAL = 'Pancho';
 DATE = 230623;
-SKIP_LOADING_DATSTRUCT = false;
+SKIP_LOADING_DATSTRUCT = true;
 % This is the human manual curation step.
 kspostprocess_manual_curate_merge(ANIMAL, DATE, SKIP_LOADING_DATSTRUCT);
+
