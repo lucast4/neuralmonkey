1328a30d3f1d2f0cc120b0b377c6621804a70f03
lucas <lucasytian@gmail.com>
2024-11-02 17:42:49 -0400

    Heatmaps of neural activity

--- neuralmonkey/neuralplots/population.py
+++ neuralmonkey/neuralplots/population.py
@@ -5,6 +5,183 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+# def _heatmap_mat(Xarr, ax):
+#     """
+#     """
+#     heatmap_mat(Xarr, ax, annotate_heatmap=False, labels_col=times_str, diverge=True, robust=True, zlims=zlims)
+
+
+        
+
+def heatmapwrapper_stratified_each_neuron_alltrials(PA, y_group_var=None):
+    """ 
+    Figure with multiple subplots, each a neuron(chan), and for that neuron, plot heatmap of FR,
+    all trials, chronological, vs. time.
+    And then overlay grouping labels on top of that (but doesnt use anything about grouping to decide how
+    to order y axis.)
+
+    Guaranteed to be sorted by trial (top is first trial)
+
+    NOTE: figure can be large and slow.
+    """
+
+    PA = PA.sort_trials_by_trialcode() # first, sort with trialcode_scal
+    dflab = PA.Xlabels["trials"]
+    X = PA.X
+    times = PA.Times
+    task_kinds = dflab["task_kind"].values
+
+    # Overlay boundaries in the plot
+    if y_group_var is not None:
+        from pythonlib.tools.pandastools import group_continuous_blocks_of_rows
+        df_sorted, _, group_boundaries = group_continuous_blocks_of_rows(dflab, y_group_var, "trialcode_scal", 
+                                                                         f"{y_group_var}_group", do_sort=False)
+        inds_start = group_boundaries["Start"].tolist()
+        groups = group_boundaries[f"{y_group_var}_group"].tolist()
+        groups_general = [dflab.iloc[i][y_group_var] for i in inds_start]
+    else:
+        inds_start = None
+        groups_general = None
+    # else:
+    #     # Then dont put groups
+
+    if False:
+        # False, since it shold already by sorted
+        from pythonlib.tools.stringtools import trialcode_to_tuple
+        trialcode_tuples = [trialcode_to_tuple(tc) for tc in dflab["trialcode"]]
+        trialcode_scalars = [trialcode_to_scalar(tc) for tc in dflab["trialcode"]]
+        sort_inds = [x[0] for x in sorted([(i, tct) for i, tct in enumerate(trialcode_tuples)], key=lambda x:x[1])]
+        Xsorted = X[:, sort_inds, :]
+        task_kinds = task_kinds[sort_inds]
+    else:
+        Xsorted = X
+
+    ncols = 6
+    n_chans = len(PA.Chans)
+    nrows = int(np.ceil(n_chans/ncols))
+    SIZE=3
+    zlims = (-1, 1)
+    fig, axes = plt.subplots(nrows, ncols, figsize=(ncols*SIZE, nrows*SIZE))
+    for i, ind_neur in enumerate(range(Xsorted.shape[0])):
+        ax = axes.flatten()[i]
+        x = Xsorted[ind_neur, :, :]
+        _heatmap_stratified_y_axis(x, times, ax, zlims=zlims, list_y_group_onsets=inds_start, list_group_labels=groups_general)
+        ax.set_title(f"{i}-{PA.Chans[i]}", color="r")
+    
+    return fig
+
+def heatmap_stratified_trials_grouped_by_neuron(PA, inds, ax, n_rand_trials=10, zlims=None):
+    """
+    Plot these trials, but showing each neuron in same plot.
+    - y, trials, grouped by neuron (and mult trials withim that nerun). Think of this as a subplot fixed on a single
+    variable level (represented by trials)
+
+    NOTE: trial order will be increasing (lowest at top)
+
+    NOTE: 
+    PARAMS:
+    - inds, rows of PA.
+    """
+    import random
+
+    times = PA.Times
+
+    # If many trials, take random subset
+    if len(inds)>n_rand_trials:
+        indsrand = random.sample(range(len(inds)), n_rand_trials)
+        inds = [inds[i] for i in indsrand]
+        inds = sorted(inds)
+
+    # Get neural data just for these trials
+    X = PA.X[:, inds, :]
+
+    # reshape to (neurons x trials, times)
+    chans = PA.Chans
+    nchans = X.shape[0]
+    ntrials = X.shape[1]
+    ntimes = X.shape[2]
+    nrows = ntrials * nchans
+    Xarr = np.reshape(X, (nchans*ntrials, ntimes))
+    neurons_row_starts = np.arange(0, nrows, ntrials).tolist()
+
+    _heatmap_stratified_y_axis(Xarr, times, ax, neurons_row_starts, chans, zlims=zlims)
+
+def heatmap_stratified_neuron_grouped_by_var(PA, ind_neur, ax, n_rand_trials, zlims, y_group_var, y_group_var_levels=None):
+    """
+    Plot this neuron, each plot:
+    - y, trials grouped by levels of y_group_var. For each group, plot a random subsample, 
+    grouped on y axis. where is sorted within y axis (based 
+    on index row within PA).
+    
+    NOTE: trial order will be increasing (lowest at top)
+    """
+    from pythonlib.tools.pandastools import grouping_append_and_return_inner_items_good
+    import random
+
+    grpdict = grouping_append_and_return_inner_items_good(PA.Xlabels["trials"], [y_group_var], y_group_var_levels, 
+                                                          must_get_all_groupouter_levels=False)
+
+    if len(grpdict)==0 or grpdict is None:
+        # no data to plot
+        return 
+    
+    times = PA.Times
+
+    # Collect data
+    list_x = []
+    list_labels = []
+    list_onset = []
+    on = 0
+    # n_rand_trials = 8
+    for grp, inds in grpdict.items():
+
+        # If many trials, take random subset
+        if len(inds)>n_rand_trials:
+            indsrand = random.sample(range(len(inds)), n_rand_trials)
+            inds = [inds[i] for i in indsrand]
+            inds = sorted(inds)
+        x = PA.X[ind_neur, inds, :]
+        list_x.append(x)
+        list_labels.extend([grp for _ in range(len(inds))])
+        list_onset.append(on)
+        on+=len(inds)
+
+    Xarr = np.concatenate(list_x, axis=0)
+    _heatmap_stratified_y_axis(Xarr, times, ax, list_onset, list(grpdict.keys()), zlims=zlims)
+
+def _heatmap_stratified_y_axis(Xarr, times, ax, 
+                               list_y_group_onsets=None, list_group_labels=None, 
+                               zlims=None):
+    """
+    PARAMS:
+    - Xarr, (nrows, ntimes), where rows represent stratified features (e..g, neuron--trial), but this function doesnt  need to know.
+    - list_y_group_onsets, list of y values that mark onset of each group, where 0 puts a line at the very top.
+    """
+    from pythonlib.tools.snstools import heatmap_mat
+
+    times_str = [f"{t:.2f}" for t in times]
+    heatmap_mat(Xarr, ax, annotate_heatmap=False, labels_col=times_str, diverge=True, robust=True, zlims=zlims)
+    ax.set_xlabel("times")
+
+    # Put lines demarcating each neuron's data onset
+
+    if list_y_group_onsets is not None:
+        for i, row in enumerate(list_y_group_onsets):
+            ax.axhline(row, color="k", linestyle="--", alpha=0.5, linewidth=1)
+    
+    # First, remove the current ticks
+    ax.tick_params(axis='y',label1On=False)
+
+    # Put labels for each group
+    if list_group_labels is not None:
+        assert len(list_y_group_onsets)==len(list_group_labels), "they need to match..."
+        from pythonlib.tools.plottools import add_secondary_axis_label_nested
+        onsets = np.array(list_y_group_onsets)
+        onset_diffs = np.diff(onsets)
+        onsets_mid = np.r_[onset_diffs, Xarr.shape[0]-onsets[-1]]/2 + onsets
+        add_secondary_axis_label_nested(ax, onsets_mid, list_group_labels, "y")
+
 def subsample_rand(X, n_rand):
     """ get random sample (dim 0), or return all if n_rand larget than X.
     """
